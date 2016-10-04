7a5fdbd207a256e192deb36c016dee8f8f722d25
arzzen <lukas.mestan@gmail.com>
2016-10-04 09:03:46 +0200

    test on private methods

--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -5,9 +5,27 @@ use src\Anagram;
 class AnagramTest extends PHPUnit_Framework_TestCase {
 
     private $anagram;
-    
+
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(TRUE);
+        return $method->invokeArgs($object, $parameters);
+    }
+
     public function setUp() {
         $this->anagram = new Anagram();
+    }
+
+    public function testSanitizeString() {
+	$value = $this->invokeMethod($this->anagram, 'sanitizeString', ['Foo bAr bAZ']);
+ 	$this->assertEquals('foobarbaz', $value);
+    }
+
+    public function testSortString() {
+	$value = $this->invokeMethod($this->anagram, 'sortString', ['c b a x']);
+	$this->assertEquals('   abcx', $value);
     }
 
     public function testSuitableString() {
